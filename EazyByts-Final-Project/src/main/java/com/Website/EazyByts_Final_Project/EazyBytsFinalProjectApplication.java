32ccfd691eb15f1cf50b4846167d178cf90a699e
bala873 <bala.v873@gmail.com>
2025-03-05 23:33:45 +0530

    first_commit

---
+++ EazyByts-Final-Project/src/main/java/com/Website/EazyByts_Final_Project/EazyBytsFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.Website.EazyByts_Final_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EazyBytsFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EazyBytsFinalProjectApplication.class, args);
+	}
+
+}
---
+++ EazyByts-Final-Project/src/test/java/com/Website/EazyByts_Final_Project/EazyBytsFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Website.EazyByts_Final_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EazyBytsFinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
